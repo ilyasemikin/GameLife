5073bc223c9192ab76cea904d03fd93ad3f37487
ilyasemikin <iasemikin@gmail.com>
2019-12-05 00:16:47 +0300

    Add project files.

---
+++ GameLife/Game.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+using static System.Math;
+
+namespace GameLife
+{
+    public class CellPoint
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int CountNeighbors { get; set; }
+
+        public CellPoint(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            CellPoint p = (CellPoint)obj;
+            return (X == p.X) && (Y == p.Y);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+    static public class GameEngine
+    {
+        static List<CellPoint> cells = new List<CellPoint>();
+
+        static private int _latency = 100;
+        static public int Width { get; set; }
+        static public int Height { get; set; }
+        static public int Latency
+        {
+            get => _latency;
+            set => _latency = (value > 10) ? value : 500;
+        }
+        static GameEngine()
+        {
+            Console.CursorVisible = false;
+        }
+        static public void AddLivingCells(CellPoint[] cls)
+        {
+            cells.AddRange(cls);
+        }
+        static public void AddLivingCell(CellPoint cell)
+        {
+            cells.Add(cell);
+        }
+        static public void AddLivingCell(int x, int y)
+        {
+            cells.Add(new CellPoint(x, y));
+        }
+
+        static public void DrawField(int x = 0, int y = 0)
+        {
+            Console.Clear();
+            foreach(var cell in cells)
+            {
+                Console.SetCursorPosition(x + cell.X, y + cell.Y);
+                Console.Write('*');
+            }
+        }
+        static public void CreateNextGeneration()
+        {
+            SetCountNeightborsCells(cells);
+            var nextGeneratironCells = cells.Where(x => x.CountNeighbors == 2 || x.CountNeighbors == 3)
+                                            .ToList();
+            foreach (var cell in cells)
+            {
+                var offsets = new (int, int)[] { (0, -1), (0, 1), (-1, 0), (1, 0) };
+                for (int i = 0; i < offsets.Length; i++)
+                {
+                    var anotherCell = new CellPoint(cell.X + offsets[i].Item1, cell.Y + offsets[i].Item2);
+                    if (anotherCell.X >= 0 && anotherCell.Y >= 0 && anotherCell.X < Width && anotherCell.Y < Height)
+                    {
+                        CalculateCountNeightborsCell(cells, anotherCell);
+                        if (anotherCell.CountNeighbors == 3 && !nextGeneratironCells.Contains(anotherCell))
+                            nextGeneratironCells.Add(anotherCell);
+                    }
+                }
+            }
+            cells = nextGeneratironCells;
+        }
+        static private void SetCountNeightborsCells(List<CellPoint> cells)
+        {
+            foreach (var cell in cells)
+                cell.CountNeighbors = CalculateCountNeightborsCell(cells, cell);
+        }
+        
+        static private int CalculateCountNeightborsCell(List<CellPoint> cells, CellPoint cell)
+        {
+            cell.CountNeighbors = cells.Where(x => Abs(cell.X - x.X) <= 1 && Abs(cell.Y - x.Y) <= 1)
+                                           .Count();
+            if (cells.Contains(cell))
+                --cell.CountNeighbors;
+            return cell.CountNeighbors;
+        }
+        static public void Run()
+        {
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    DrawField();
+                    CreateNextGeneration();
+                    Thread.Sleep(Latency);
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
---
+++ GameLife/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameEngine.AddLivingCell(1, 1);
+            GameEngine.AddLivingCell(1, 2);
+            GameEngine.AddLivingCell(1, 3);
+            GameEngine.AddLivingCell(10, 10);
+            GameEngine.AddLivingCell(10, 11);
+            GameEngine.AddLivingCell(11, 10);
+            GameEngine.AddLivingCell(11, 11);
+            GameEngine.Width = Console.WindowWidth;
+            GameEngine.Height = Console.WindowHeight;
+            GameEngine.Latency = 250;
+            GameEngine.Run();
+        }
+    }
+}
